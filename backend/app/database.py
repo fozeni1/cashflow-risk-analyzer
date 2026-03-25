23c14a271b0d286638035afb3e9bacba9b04b4f2
fozeni1 <yabloko0718egor@yandex.ru>
2026-03-25 09:06:03 +0300

    Update wallets.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,3 @@
-# Импортируем функции для работы с базой данных из SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -6,7 +5,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "sqlite:///./finance.db"
 
 # Создаем движок для подключения к базе данных
-# check_same_thread=False позволяет использовать SQLite в многопоточной среде
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 # Создаем фабрику для создания сессий работы с базой данных
